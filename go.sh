b328db67eab57f2e0df218cef62b28ac49c43dae
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-01-22 11:45:19 -0800

    Open source

--- go.sh
+++ go.sh
@@ -3,5 +3,5 @@ CLASSPATH=../craftbukkit-1.1-R1-SNAPSHOT.jar javac *.java -Xlint:unchecked -Xlin
 rm -rf me 
 mkdir -p me/exphc/SilkSpawners
 mv *.class me/exphc/SilkSpawners
-jar cf SilkSpawners.jar me/ *.yml 
+jar cf SilkSpawners.jar me/ *.yml *.java
 cp SilkSpawners.jar ../plugins/
