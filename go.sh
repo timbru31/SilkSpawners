98520c170d2a3399a6b6229d643259f53c8827f6
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-02-26 08:45:42 -0800

    Compile for 1.1-R4

--- go.sh
+++ go.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
-CLASSPATH=../craftbukkit-1.1-R3.jar javac *.java -Xlint:unchecked -Xlint:deprecation
+CLASSPATH=../craftbukkit-1.1-R4.jar javac *.java -Xlint:unchecked -Xlint:deprecation
 rm -rf me 
 mkdir -p me/exphc/SilkSpawners
 mv *.class me/exphc/SilkSpawners
 jar cf SilkSpawners.jar me/ *.yml *.java ChangeLog README.md LICENSE
-cp SilkSpawners.jar ../plugins/
+#cp SilkSpawners.jar ../plugins/
