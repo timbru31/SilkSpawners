d4c2732e736537c1d3b5736009788d85c42a5c73
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2013-03-21 00:35:01 +0100

    Update: MC Version

--- src/main/java/de/dustplanet/silkspawners/SilkSpawners.java
+++ src/main/java/de/dustplanet/silkspawners/SilkSpawners.java
@@ -53,7 +53,7 @@ public class SilkSpawners extends JavaPlugin {
     public boolean spoutEnabled, usePermissions;
     public CommentedConfiguration config, localization, mobs;
     private File configFile, localizationFile, mobsFile;
-    public static final String COMPATIBLE_MINCERAFT_VERSION = "1.5";
+    public static final String COMPATIBLE_MINCERAFT_VERSION = "1.5.1";
 
     public void onDisbale() {
 	su.clearAll();
