1c70c2156c3b8a702099f1b2cb381aebf11e8fbc
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2013-05-17 01:33:29 +0200

    Updated: Event with CodingConventions

--- src/main/java/de/dustplanet/silkspawners/events/SilkSpawnersSpawnerChangeEvent.java
+++ src/main/java/de/dustplanet/silkspawners/events/SilkSpawnersSpawnerChangeEvent.java
@@ -8,84 +8,84 @@ import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 public class SilkSpawnersSpawnerChangeEvent extends Event implements Cancellable {
-	private static final HandlerList handlers = new HandlerList();
-	private boolean cancelled;
-	// Our objects
-	private Player player;
-	private short id;
-	private Block block;
-	private CreatureSpawner spawner;
+    private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled;
+    // Our objects
+    private Player player;
+    private short id;
+    private Block block;
+    private CreatureSpawner spawner;
 
-	public SilkSpawnersSpawnerChangeEvent(Player player, Block block, short id) {
-		this.player = player;
-		this.block = block;
-		if (block != null) {
-			this.spawner = (CreatureSpawner) block.getState();
-		}
-		this.id = id;
+    public SilkSpawnersSpawnerChangeEvent(Player player, Block block, short id) {
+	this.player = player;
+	this.block = block;
+	if (block != null) {
+	    this.spawner = (CreatureSpawner) block.getState();
 	}
-	
-	/**
-	 * Determine if the event is cancelled or not
-	 * @return yes or no
-	 */
-	public boolean isCancelled() {
-		return this.cancelled;
-	}
-	
-	/**
-	 * Cancel the event
-	 * @param boolean - whether the event should be cancelled or not
-	 */
-	public void setCancelled(boolean cancel) {
-		this.cancelled = cancel;
-	}
+	this.id = id;
+    }
 
-	/**
-	 * Get the player from this event
-	 * @return the player object
-	 */
-	public Player getPlayer() {
-		return this.player;
-	}
-	
-	/**
-	 * Get the block of this event
-	 * @return the block - in this case a spawner; returns null when an egg is used
-	 */
-	public Block getBlock() {
-		return this.block;
-	}
-	
-	/**
-	 * Get the creature spawner of this event
-	 * @return the creature spawner; returns null when an egg is used
-	 */
-	public CreatureSpawner getSpawner() {
-		return this.spawner;
-	}
-	
-	/**
-	 * Get the entity ID (mob to spawn) from this event
-	 * @return the entity ID
-	 */
-	public short getEntityID() {
-		return this.id;
-	}
-	
-	/**
-	 * Sets the entityID of the spawner
-	 * @param short - the new entityID
-	 */
-	public void setEntityID(short id) {
-		this.id = id;
-	}
+    /**
+     * Determine if the event is cancelled or not
+     * @return yes or no
+     */
+    public boolean isCancelled() {
+	return this.cancelled;
+    }
 
-	public HandlerList getHandlers() {
-		return handlers;
-	}
+    /**
+     * Cancel the event
+     * @param boolean - whether the event should be cancelled or not
+     */
+    public void setCancelled(boolean cancel) {
+	this.cancelled = cancel;
+    }
 
-	public static HandlerList getHandlerList() {
-		return handlers;
-	}
+    /**
+     * Get the player from this event
+     * @return the player object
+     */
+    public Player getPlayer() {
+	return this.player;
+    }
+
+    /**
+     * Get the block of this event
+     * @return the block - in this case a spawner; returns null when an egg is used
+     */
+    public Block getBlock() {
+	return this.block;
+    }
+
+    /**
+     * Get the creature spawner of this event
+     * @return the creature spawner; returns null when an egg is used
+     */
+    public CreatureSpawner getSpawner() {
+	return this.spawner;
+    }
+
+    /**
+     * Get the entity ID (mob to spawn) from this event
+     * @return the entity ID
+     */
+    public short getEntityID() {
+	return this.id;
+    }
+
+    /**
+     * Sets the entityID of the spawner
+     * @param short - the new entityID
+     */
+    public void setEntityID(short id) {
+	this.id = id;
+    }
+
+    public HandlerList getHandlers() {
+	return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+	return handlers;
+    }
 }
