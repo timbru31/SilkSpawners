6f71d841d63367046f52679c071f00ce9ef9a6fb
hcherndon <hcherndon@gmail.com>
2014-04-13 20:42:57 -0500

    Fix a major dupelication glitch while having a SilkSpawner in your hand, and breaking plants. Dupelicates items, and if McMMO is installed, infinite levels are included. To xGhOsTkiLLeRx, EventPriority.MONITOR is fired after the event is executed and should not be used to actually modify the outcome of an event.

--- src/main/java/de/dustplanet/silkspawners/listeners/SilkSpawnersBlockListener.java
+++ src/main/java/de/dustplanet/silkspawners/listeners/SilkSpawnersBlockListener.java
@@ -33,7 +33,7 @@ public class SilkSpawnersBlockListener implements Listener {
 	su = util;
     }
 
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent event) {
 	if (event.isCancelled()) {
 	    return;
